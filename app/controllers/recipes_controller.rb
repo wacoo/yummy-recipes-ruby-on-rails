b26c10541630f97ab3c252c0fbc75af738516764
wac <wabaham9@gmail.com>
2023-11-21 13:57:14 +0300

    Add delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,41 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+  layout 'application'
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+        redirect_to @recipe, notice: 'Recipe created successfully.'
+    else
+        flash.now[:error] = 'Recipe not created. Try again!'
+        render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    puts @recipe
+    redirect_to recipes_path, alert: "Recipe deleted successfully."
+    # else
+    #     puts 'DDDD'
+    #     redirect_to recipes_path,  alert: "Failed to delete the recipe #{@recipe.id}"
+    #end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
