a2ae26fe7ef47457cba330d7ed8a197e94b463e0
DevaWinner <winneraaniekan@gmail.com>
2023-11-22 10:24:25 +0100

    Fix Linter Errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,31 +1,35 @@
 class FoodsController < ApplicationController
-    before_action :authenticate_user!
-    def index
-        @foods = Food.all
+  before_action :authenticate_user!
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to @food, notice: 'Food created successfully.'
+    else
+      flash[:error] = 'Failed to create food.'
+      render :new
     end
-    def new
-        @food = Food.new
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.user = current_user
+    if @food.destroy
+      redirect_to foods_path, notice: 'Food removed successfully.'
+    else
+      redirect_to foods_path, error: 'Failed to remove food.'
     end
-    def create
-        @food = Food.new(food_params)
-        @food.user = current_user
-        if @food.save
-            redirect_to @food, notice: 'Food created successfully.'
-          else
-            flash[:error] = 'Failed to create food.'
-            render :new
-          end
-    end
-    def destroy
-        @food = Food.find(params[:id])
-        @food.user = current_user
-        if @food.destroy
-            redirect_to foods_path, notice: "Food removed successfully."
-        else
-            redirect_to foods_path, error: "Failed to remove food."
-        end
-    end
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-    end
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
