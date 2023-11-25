92bd6dd442299b2495bcf041e6556c0a849c72eb
wac <wabaham9@gmail.com>
2023-11-25 09:57:47 +0300

    Add more changes

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_25_051111) do
   create_table "foods_recipes", id: false, force: :cascade do |t|
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
-    t.float "quantity"
     t.index ["food_id", "recipe_id"], name: "index_foods_recipes_on_food_id_and_recipe_id"
     t.index ["recipe_id", "food_id"], name: "index_foods_recipes_on_recipe_id_and_food_id"
   end
